a9b05184938c478debdaa82f14f6975d9d9ad013
Root(김명석) <mskim@gabia.com>
2021-07-05 10:47:23 +0900

    modify: lombok dependency 추가

--- src/test/java/com/gabia/apigateway/request/Raw.java
+++ src/test/java/com/gabia/apigateway/request/Raw.java
@@ -1,24 +1,13 @@
 package com.gabia.apigateway.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class Raw {
     private String appName;
     private List<String> address;
-
-    public String getAppName() {
-        return appName;
-    }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
-
-    public List<String> getAddress() {
-        return address;
-    }
-
-    public void setAddress(List<String> address) {
-        this.address = address;
-    }
 }
--- src/test/java/com/gabia/apigateway/response/Conversation.java
+++ src/test/java/com/gabia/apigateway/response/Conversation.java
@@ -1,27 +1,13 @@
 package com.gabia.apigateway.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Conversation {
     private String id;
     private String name;
-
-    public Conversation(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
